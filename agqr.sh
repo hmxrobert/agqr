e1c51f036d2687b5fcc5267fe742090156cffadd
Tsuruno Sato <hmx_robert@hotmail.com>
2018-12-28 13:50:24 +0900

    Update agqr.sh

--- agqr.sh
+++ agqr.sh
@@ -1,3 +1,3 @@
 #!/bin/ash
-/usr/bin/ruby /agqr.rb $4
-/usr/bin/ruby /makepodcast.rb $1 $2 $3
+/usr/bin/ruby /agqr.rb $2
+/usr/bin/ruby /makepodcast.rb "超A＆G＋" $1 "/mnt/agqr/data/mp3/" > /usr/share/nginx/html/podcast.rss
