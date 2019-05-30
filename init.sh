10d47cf9502ad95a96e9edf9357712c5c90d954e
Tsuruno Sato <hmx_robert@hotmail.com>
2019-05-30 17:09:20 +0900

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 #!/bin/ash
 /usr/sbin/nginx &
-cp /var/spool/crontabs/root.org /var/spool/cron/crontabs/root
+cp /var/spool/cron/crontabs/root.org /var/spool/cron/crontabs/root
 echo "29,59 * * * * /agqr.sh $1 $2 $3" >> /var/spool/cron/crontabs/root
 crond -f -d 8 
